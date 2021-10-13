ed208422049e6bc758993860c7b044fa3be9f83d
Matheus002 <matheus002_@hotmail.com>
2021-10-13 06:32:53 -0300

    feat: add CreateClients migration table

--- src/db/migrations/1634093220774-CreateClients.ts
+++ src/db/migrations/1634093220774-CreateClients.ts
@@ -1,8 +1,44 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateClients1634093220774 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "clients",
+                columns: [  
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "nome",
+                        type: "varchar"
+                    },
+                    {
+                        name: "cpf",
+                        type: "varchar"
+                    },
+                    {
+                        name: "email",
+                        type: "varchar"
+                    },
+                    {
+                        name: "telefone",
+                        type: "varchar"
+                    },
+                    {
+                        name: "sexo",
+                        type: "varchar"
+                    },
+                    {
+                        name: "DataNascimento",
+                        type: "varchar"
+                    }
+                ],
+            )}        
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
